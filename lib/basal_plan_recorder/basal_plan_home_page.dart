e7f18e5e3b585cd72066070b5d67c243057a70a9
Eran Cohen <ceranco@gmail.com>
2020-05-05 17:22:50 +0300

    Changed `BasalPlanHomePage` to work using a "batch" mode.
    * Added `editing` flag to indicate current mode.
    * Replaced the change listener on the plan (which updated the database)
    with a end-of-edit function.
    * Changed the basal plan collection name and the current plan document
    name.

--- lib/basal_plan_recorder/basal_plan_home_page.dart
+++ lib/basal_plan_recorder/basal_plan_home_page.dart
@@ -1,11 +1,13 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:multitool/basal_plan_recorder/models/basal_plan.dart';
 import 'package:multitool/basal_plan_recorder/widgets/basal_plan_list_view.dart';
 import 'package:multitool/basal_plan_recorder/widgets/edit_segment_bottom_sheet.dart';
+import 'package:multitool/basal_plan_recorder/widgets/hiding_progress_indicator.dart';
 import 'package:provider/provider.dart';
+
+const basalPlansCollectionName = 'basal-plans';
+const currentPlanDocumentName = 'current';
 
 class BasalPlanHomePage extends StatefulWidget {
   @override
@@ -13,7 +15,10 @@ class BasalPlanHomePage extends StatefulWidget {
 }
 
 class _BasalPlanHomePageState extends State<BasalPlanHomePage> {
+  bool editing = false;
+  BasalPlan originalPlan;
   BasalPlan plan;
+  DocumentReference currentPlanDocument;
 
   @override
   void initState() {
@@ -22,54 +27,92 @@ class _BasalPlanHomePageState extends State<BasalPlanHomePage> {
   }
 
   void getPlan() async {
-    const collectionName = 'basalplans';
-    const documentName = 'plan';
-    final document =
-        Firestore.instance.collection(collectionName).document(documentName);
-    final snapshot = await document.get();
+    currentPlanDocument = Firestore.instance
+        .collection(basalPlansCollectionName)
+        .document(currentPlanDocumentName);
+    final snapshot = await currentPlanDocument.get();
 
     setState(() {
       plan = BasalPlan.fromJson(snapshot.data);
-    });
-    plan.addListener(() async {
-      await document.setData(plan.json);
+      originalPlan = BasalPlan.copy(plan);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // final encoder = JsonEncoder.withIndent('  ');
-    // print(encoder.convert(plan.json));
-    if (plan == null) {
-      return CircularProgressIndicator();
-    }
+    final fabIcon = editing ? Icons.save : Icons.edit;
+    // final fabOnPressed = editing
+    //     ? openAddSegmentModal
+    //     : () {
+    //         setState(() {
+    //           editing = true;
+    //         });
+    //       };
+    final fabOnPressed = () {
+      // Save the new plan if it changed.
+      if (editing && originalPlan != plan) {
+        currentPlanDocument.setData(plan.json);
+        originalPlan = BasalPlan.copy(plan);
+      }
+
+      setState(() {
+        editing = !editing;
+      });
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Basal Plan'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 4.0),
-        child: ChangeNotifierProvider.value(
-          value: plan,
-          child: BasalPlanListView(),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) => EditSegmentBottomSheet(
-              onFinish: (segment) => setState(() {
-                plan.add(segment);
-              }),
+      body: HidingProgressIndicator(
+        inProgress: plan == null,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 4.0),
+          child: ChangeNotifierProvider.value(
+            value: plan,
+            child: BasalPlanListView(
+              editable: editing,
             ),
-          );
-        },
-        child: Icon(
-          Icons.add,
-          size: 30,
+          ),
         ),
       ),
+      floatingActionButton: plan != null
+          ? Row(
+              textDirection: TextDirection.rtl,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: fabOnPressed,
+                  child: Icon(
+                    fabIcon,
+                    size: 30,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                AnimatedOpacity(
+                  opacity: editing ? 1.0 : 0.0,
+                  duration: Duration(milliseconds: 250),
+                  child: FloatingActionButton(
+                    onPressed: editing ? openAddSegmentModal : null,
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : null,
     );
   }
+
+  void openAddSegmentModal() => showModalBottomSheet(
+        context: context,
+        builder: (context) => EditSegmentBottomSheet(
+          onFinish: (segment) => setState(() {
+            plan.add(segment);
+          }),
+        ),
+      );
 }
