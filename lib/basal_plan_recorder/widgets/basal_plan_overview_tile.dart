53cd835474e4ad207ef6f14a470a7498a62f62eb
Eran Cohen <ceranco@gmail.com>
2020-05-20 19:06:22 +0300

    Added `onTap` and `onLongPress` to `BasalPlanOverviewTile`.

--- lib/basal_plan_recorder/widgets/basal_plan_overview_tile.dart
+++ lib/basal_plan_recorder/widgets/basal_plan_overview_tile.dart
@@ -6,48 +6,57 @@ class BasalPlanOverviewTile extends StatelessWidget {
   final BasalPlan plan;
   final double size;
 
+  final void Function() onTap;
+  final void Function() onLongPress;
+
   const BasalPlanOverviewTile({
     @required this.plan,
     @required this.size,
+    this.onTap,
+    this.onLongPress,
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Container(
-        width: size,
-        height: size,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Text(
-                  plan.created.dmyHms,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
+    return Material(
+      elevation: 5,
+      color: Colors.blue,
+      clipBehavior: Clip.antiAlias,
+      borderRadius: BorderRadius.circular(10),
+      child: InkWell(
+        onTap: onTap,
+        onLongPress: onLongPress,
+        child: SizedBox(
+          width: size,
+          height: size,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    plan.created.dmyHms,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: BasalPlanChart(plan: plan),
-              ),
-            )
-          ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IgnorePointer(
+                    child: BasalPlanChart(plan: plan),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
