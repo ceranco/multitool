7b038a5c70ee86bf3cd5e2fa554a486ad7d74073
Eran Cohen <ceranco@gmail.com>
2020-05-25 16:14:31 +0300

    Added `BasalPlanPage`, which shows an "overview" of a specific plan.
    The page shows a chart of the plan and then a listview of the segments
    in readonly mode.

---
+++ lib/basal_plan_recorder/pages/basal_plan_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:multitool/basal_plan_recorder/extensions/date_time_extensions.dart';
+import 'package:multitool/basal_plan_recorder/models/basal_plan.dart';
+import 'package:multitool/basal_plan_recorder/widgets/basal_plan_chart.dart';
+import 'package:multitool/basal_plan_recorder/widgets/basal_plan_list_view.dart';
+import 'package:provider/provider.dart';
+
+class BasalPlanPage extends StatelessWidget {
+  final BasalPlan plan;
+
+  const BasalPlanPage({@required this.plan, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(plan.created.dmyHms),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 2.0,
+                  spreadRadius: 0.0,
+                )
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 32.0,
+              ).copyWith(bottom: 8.0),
+              child: BasalPlanChart(
+                plan: plan,
+                lineColor: Colors.blue,
+                gridColor: Colors.black26,
+                titleStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ChangeNotifierProvider<BasalPlan>.value(
+              value: plan,
+              child: BasalPlanListView(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
